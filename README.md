beff2f20a1cd0ed314aa07e095140d54a9eae0d6
janardhanbonu <janardhanbonu@gmail.com>
2018-10-16 07:41:51 +0530

    postgresql get tables list and row count

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # dbstatusreporter
+
+This API is used to report the status of tables in database
--- src/main/java/com/report/database/dbstatusreporter/JDBCPostgresql.java
+++ src/main/java/com/report/database/dbstatusreporter/JDBCPostgresql.java
@@ -11,73 +11,68 @@ import static java.sql.DriverManager.getConnection;
 public class JDBCPostgresql {
 
     public static void main( String args[] ) throws SQLException {
-        Connection c = null;
-        Statement stmt = null;
+
+        String jdbcUrl = "jdbc:postgresql://localhost:5432/bookdepot";
+        String user = "bookdepot";
+        String pwd = "bookdepot";
 
         List<Tables> tables = new ArrayList<>();
-
         try {
             Class.forName("org.postgresql.Driver");
-            c = getConnection("jdbc:postgresql://localhost:5432/bookdepot",
-                            "bookdepot", "bookdepot");
-            c.setAutoCommit(false);
-            System.out.println("Opened database successfully");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
 
-            stmt = c.createStatement();
-            ResultSet rs = stmt.executeQuery( "SELECT * FROM pg_catalog.pg_tables  pt where pt.schemaname = 'bookdepotschema';" );
+        String allTablesQuery = "SELECT * FROM pg_catalog.pg_tables  pt where pt.schemaname = 'bookdepotschema';";
 
-
-
+        try (Connection c = getConnection(jdbcUrl, user, pwd);
+             Statement stmt = c.createStatement();
+             ResultSet rs = stmt.executeQuery( allTablesQuery );
+        ){
             while ( rs.next() ) {
-
-
                 String  schemaname = rs.getString("schemaname");
                 String  tablename = rs.getString("tablename");
                 String  tableowner = rs.getString("tableowner");
                 String  tablespace = rs.getString("tablespace");
                 Tables  table = new Tables(schemaname, tablename, tableowner, tablespace);
-//                System.out.println( "ID = " + schemaname );
-//                System.out.println( "NAME = " + tablename );
-//                System.out.println( "AGE = " + tableowner );
-//                System.out.println( "ADDRESS = " + tablespace );
-//                System.out.println();
-            tables.add(table);
-
+                tables.add(table);
             }
             rs.close();
             stmt.close();
-            c.close();
+
         } catch ( Exception e ) {
             System.err.println( e.getClass().getName()+": "+ e.getMessage() );
             System.exit(0);
+        } finally{
+
         }
 
-        try{
-            Class.forName("org.postgresql.Driver");
-            c = getConnection("jdbc:postgresql://localhost:5432/bookdepot",
-                    "bookdepot", "bookdepot");
-            c.setAutoCommit(false);
-            System.out.println("Opened database successfully");
+        try(
+            Connection c = getConnection(jdbcUrl, user, pwd);
+            ){
 
             for (Tables table: tables) {
-                stmt = c.createStatement();
-                ResultSet rs = stmt.executeQuery( "SELECT count(*) as ROW_COUNT FROM "+table.getSchemaname()+"."+table.getTablename() );
+                try( Statement stmt = c.createStatement();
+                     ResultSet rs = stmt.executeQuery( "SELECT count(*) as ROW_COUNT FROM "+table.getSchemaname()+"."+table.getTablename() );){
+
                 while ( rs.next() ) {
                     String  rowCount = rs.getString("ROW_COUNT");
                     System.out.println( table.getSchemaname()+"."+table.getTablename()+" -> " + rowCount );
                 }
-                rs.close();
-                stmt.close();
+                } catch ( Exception e ) {
+                    System.err.println( e.getClass().getName()+": "+ e.getMessage() );
+                    System.exit(0);
+                }
+
             }
 
         } catch ( Exception e ) {
             System.err.println( e.getClass().getName()+": "+ e.getMessage() );
             System.exit(0);
         } finally {
-            c.close();
+
         }
 
 
-        System.out.println("Operation done successfully");
     }
 }
